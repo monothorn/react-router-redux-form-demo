a282d3f1a8381dca0fb95751d53afd61ee1ae9e9
Saurabh Rayakwar <saurabh.r@stylabs.in>
2017-12-29 20:28:18 +0530

    update state after deletion

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import {FETCH_POSTS, FETCH_POST} from "../actions/index";
+import {FETCH_POSTS, FETCH_POST, DELETE_POST} from "../actions/index";
 
 export default function (state = {}, action) {
     switch (action.type) {
@@ -9,6 +9,9 @@ export default function (state = {}, action) {
             // newState[post.id] = post;
             // return newState;
             return {...state, [post.id]: post};//key interpolation es6
+
+        case DELETE_POST:
+            return _.omit(state, action.payload);
         case FETCH_POSTS:
             console.log(action.payload.data);
             return _.mapKeys(action.payload.data, 'id');//array to object
